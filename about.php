44ec7644516c417f53b5fcb039e19bc90f8c32e4
Md Golam Sarowar <Sojolewu6@gmail.com>
2019-06-30 09:39:33 +0600

    Add files via upload

---
+++ about.php
@@ -0,0 +1,68 @@
+
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<title>E-HealthCare</title>
+		<link href="css/style.css" rel="stylesheet" type="text/css"  media="all" />
+		<link href='http://fonts.googleapis.com/css?family=Ropa+Sans' rel='stylesheet' type='text/css'>
+		<link rel="stylesheet" href="css/responsiveslides.css">
+		<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
+		<script src="js/responsiveslides.min.js"></script>
+		  <script>
+		    // You can also use "$(window).load(function() {"
+			    $(function () {
+			
+			      // Slideshow 1
+			      $("#slider1").responsiveSlides({
+			        maxwidth: 1600,
+			        speed: 600
+			      });
+			});
+		  </script>
+	</head>
+	<body>
+	
+		
+		<!--start-wrap-->
+		
+			<!--start-header-->
+			<div class="header">
+				<div class="wrap">
+				<!--start-logo-->
+				<!--end-logo-->
+				<!--start-top-nav-->
+				<div class="top-nav">
+					<ul>
+						<li><a href="index.html">Home</a></li>
+						
+						<li><a href="contact.php">Contact</a></li>
+					</ul>					
+				</div>
+				<div class="clear"> </div>
+				<!--end-top-nav-->
+			</div>
+			<!--end-header-->
+		</div>
+		<div class="clear"> </div>
+			<!--start-image-slider---->
+					<div class="image-slider">
+						<!-- Slideshow 1 -->
+					    <ul class="rslides" id="slider1">
+					      <li><img src="images/slider1.jpg" alt=""></li>
+					      <li><img src="images/slider2.jpg" alt=""></li>
+					      <li><img src="images/slider3.jpg" alt=""></li>
+					    </ul>
+						 <!-- Slideshow 2 -->
+					</div>
+					<!--End-image-slider---->
+		    <div class="clear"> </div>
+		   
+		   
+		   <h1> Moto </h1>
+		   <p align="right justified"> Since present era stepping towards revolution of machine to machine communication rather than human to human. Therefore, this new trend has significant point of view from the perspective of medical science. Realizing the worth of IoT in healthcare, in this study an online based platform will be develop along with the privilege to store necessary data for further proceedings. This online healthcare platform will monitor patients every second conditions and remain active for 24 hours which will help taking immediate measures in case of various unexpected situations.  </p>
+		   
+		<!--end-wrap-->
+		
+	</body>
+</html>
+
---
+++ contact1.php
@@ -0,0 +1,86 @@
+
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<title>E-HealthCare | Contact us</title>
+		<link href="css/style.css" rel="stylesheet" type="text/css"  media="all" />
+		<link href='http://fonts.googleapis.com/css?family=Ropa+Sans' rel='stylesheet' type='text/css'>
+	</head>
+	<body>
+		<!--start-wrap-->
+		
+			<!--start-header-->
+			<div class="header">
+				<div class="wrap">
+				<!--start-logo-->
+				<!--end-logo-->
+				<!--start-top-nav-->
+				<div class="top-nav">
+					<ul>
+						<li><a href="index.html">Home</a></li>
+					
+						<li><a href="about.php">About</a></li>
+					</ul>					
+				</div>
+				<div class="clear"> </div>
+				<!--end-top-nav-->
+			</div>
+			<!--end-header-->
+		</div>
+		    <div class="clear"> </div>
+		   <div class="wrap">
+		   	<div class="contact">
+		   	<div class="section group">				
+				
+				<div class="col span_2_of_3">
+				  <div class="contact-form">
+				  	<h2>Contact Us For Help</h2>
+					    <form>
+					    	<div>
+						    	<span><label>NAME</label></span>
+						    	<span><input type="text" value=""></span>
+						    </div>
+						    <div>
+						    	<span><label>E-MAIL</label></span>
+						    	<span><input type="email" value=""></span>
+						    </div>
+						    <div>
+						     	<span><label>MOBILE.NO</label></span>
+						    	<span><input type="tel" value=""></span>
+						    </div>
+						    <div>
+						    	<span><label>SUBJECT</label></span>
+						    	<span><textarea> </textarea></span>
+						    </div>
+						   <div>
+						   		<span><input type="submit" value="Submit"></span>
+						  </div>
+					    </form>
+				    </div>
+  				</div>
+
+
+
+				
+				<div class="col span_1_of_3">
+					
+      			<div class="company_address">
+				     	<h2>BD E-HealthCare:</h2>
+						    	<p>Mobile: 01711xxxxxxx</p>
+						   		<p>Phone: +071938383</p>
+				   		<p>Fax: (000) 000 00 00 0</p>
+				 	 	<p>Email: BD_healthcare@ehk.com</p>
+				   	
+				   </div>
+				</div>	
+
+				
+			  </div>
+			  	 <div class="clear"> </div>
+	</div>
+	<div class="clear"> </div>
+			</div>
+	      
+	</body>
+</html>
+

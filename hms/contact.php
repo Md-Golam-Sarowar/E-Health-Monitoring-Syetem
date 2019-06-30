5ae50d682e6e05a4c415c1ce05a26d885dccfea0
Md Golam Sarowar <Sojolewu6@gmail.com>
2019-06-30 09:40:26 +0600

    Add files via upload

---
+++ hms/contact.php
@@ -0,0 +1,91 @@
+
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<title>E-HealthCare | Contact us</title>
+		<link href="css/style.css" rel="stylesheet" type="text/css"  media="all" />
+		<link href='http://fonts.googleapis.com/css?family=Ropa+Sans' rel='stylesheet' type='text/css'>
+	</head>
+	<body>
+		<!--start-wrap-->
+		
+			<!--start-header-->
+			<div class="header">
+				<div class="wrap">
+				<!--start-logo-->
+				<div class="logo">
+					<a href="index.html" style="font-size: 30px;"> <img src="images/logo.gif"/> </a>
+				</div>
+				<!--end-logo-->
+				<!--start-top-nav-->
+				<div class="top-nav">
+					<ul>
+						<li><a href="index.html">Home</a></li>
+					
+						<li><a href="about.php">About US</a></li>
+					</ul>					
+				</div>
+				<div class="clear"> </div>
+				<!--end-top-nav-->
+			</div>
+			<!--end-header-->
+		</div>
+		    <div class="clear"> </div>
+		   <div class="wrap">
+		   	<div class="contact">
+		   	<div class="section group">				
+				
+				<div class="col span_2_of_3">
+				  <div class="contact-form">
+				  	<h2>Contact Us</h2>
+					    <form>
+					    	<div>
+						    	<span><label>NAME</label></span>
+						    	<span><input type="text" value=""></span>
+						    </div>
+						    <div>
+						    	<span><label>E-MAIL</label></span>
+						    	<span><input type="email" value=""></span>
+						    </div>
+						    <div>
+						     	<span><label>MOBILE.NO</label></span>
+						    	<span><input type="tel" value=""></span>
+						    </div>
+						    <div>
+						    	<span><label>SUBJECT</label></span>
+						    	<span><textarea> </textarea></span>
+						    </div>
+						   <div>
+						   		<span><input type="submit" value="Submit"></span>
+						  </div>
+					    </form>
+				    </div>
+  				</div>
+
+
+
+				
+				<div class="col span_1_of_3">
+					
+      			<div class="company_address">
+				     	<h2>BD E-HealthCare:</h2>
+						    	<p>Jahurul Islam City,</p>
+						   		<p>Aftabnagar, Rampura,</p>
+						   		<p>Dhaka, Bangladesh.</p>
+				   		<p>Phone: (+880) 1712420420</p>
+				   		<p>Fax: (000) 000 00 00 0</p>
+				 	 	<p>Email: <span>BD_healthcare@ehk.com</span></p>
+				   	
+				   </div>
+				</div>	
+
+				
+			  </div>
+			  	 <div class="clear"> </div>
+	</div>
+	<div class="clear"> </div>
+			</div>
+	      
+	</body>
+</html>
+
---
+++ hms/registration.php
@@ -0,0 +1,157 @@
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+	<head>
+		<title>User Registration</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0, minimum-scale=1.0, maximum-scale=1.0">
+		<meta name="apple-mobile-web-app-capable" content="yes">
+		<meta name="apple-mobile-web-app-status-bar-style" content="black">
+		<meta content="" name="description" />
+		<meta content="" name="author" />
+		<link href="http://fonts.googleapis.com/css?family=Lato:300,400,400italic,600,700|Raleway:300,400,500,600,700|Crete+Round:400italic" rel="stylesheet" type="text/css" />
+		<link rel="stylesheet" href="vendor/bootstrap/css/bootstrap.min.css">
+		<link rel="stylesheet" href="vendor/fontawesome/css/font-awesome.min.css">
+		<link rel="stylesheet" href="vendor/themify-icons/themify-icons.min.css">
+		<link href="vendor/animate.css/animate.min.css" rel="stylesheet" media="screen">
+		<link href="vendor/perfect-scrollbar/perfect-scrollbar.min.css" rel="stylesheet" media="screen">
+		<link href="vendor/switchery/switchery.min.css" rel="stylesheet" media="screen">
+		<link rel="stylesheet" href="assets/css/styles.css">
+		<link rel="stylesheet" href="assets/css/plugins.css">
+		<link rel="stylesheet" href="assets/css/themes/theme-1.css" id="skin_color" />
+		<link href="css/style.css" rel="stylesheet" type="text/css"  media="all" />
+
+		
+		
+		
+
+	</head>
+
+	
+	<body class="login">
+	
+	<div class="header">
+				<div class="wrap">
+				<!--start-logo-->
+				
+				<!--end-logo-->
+				<!--start-top-nav-->
+				<div class="top-nav">
+					<ul>
+						<li><a href="../index.html">Home</a></li>
+						
+						<li><a href="contact.php">Contact</a></li>
+						<li><a href="about.php">About</a></li>
+					</ul>					
+				</div>
+				<div class="clear"> </div>
+				<!--end-top-nav-->
+			</div>
+			<!--end-header-->
+		</div>
+		<div class="clear"> </div>
+	
+	
+		<!-- start: REGISTRATION -->
+		<div class="row">
+			<div class="main-login col-xs-10 col-xs-offset-1 col-sm-8 col-sm-offset-2 col-md-4 col-md-offset-4">
+				<div class="logo margin-top-30">
+				<h2>Device Registration</h2>
+				</div>
+						<div class="clear"> </div>
+
+				<!-- start: REGISTER BOX -->
+				<div class="box-register">
+					<form name="registration" id="registration"  method="post">
+						<fieldset>
+							
+							<p>
+								Enter your personal details below:
+							</p>
+							<div class="form-group">
+								<input type="text" class="form-control" name="full_name" placeholder="Full Name" required>
+							</div>
+							<div class="form-group">
+								<input type="text" class="form-control" name="address" placeholder="Address" required>
+							</div>
+							<div class="form-group">
+								<input type="text" class="form-control" name="city" placeholder="City" required>
+							</div>
+							<div class="form-group">
+								<label class="block">
+									Gender
+								</label>
+								<div class="clip-radio radio-primary">
+									<input type="radio" id="rg-female" name="gender" value="female" >
+									<label for="rg-female">
+										Female
+									</label>
+									<input type="radio" id="rg-male" name="gender" value="male">
+									<label for="rg-male">
+										Male
+									</label>
+								</div>
+							</div>
+							<p>
+								Enter Device details below:
+							</p>
+							<div class="form-group">
+								<span class="input-icon">
+									<input type="email" class="form-control" name="name" id="name" onBlur="userAvailability()"  placeholder="Device Name" required>
+									<i class="fa fa-envelope"></i> </span>
+									 <span id="user-availability-status1" style="font-size:12px;"></span>
+							</div>
+							
+							<div class="form-group">
+								<span class="input-icon">
+									<input type="email" class="form-control" name="IP" id="IP" onBlur="userAvailability()"  placeholder="IP address" required>
+									<i class="fa fa-envelope"></i> </span>
+									 <span id="user-availability-status1" style="font-size:12px;"></span>
+							</div>
+							
+							<div class="form-group">
+								<span class="input-icon">
+									<input type="password" class="form-control" id="password" name="password" placeholder="Password" required>
+									<i class="fa fa-lock"></i> </span>
+							</div>
+							<div class="form-group">
+								<span class="input-icon">
+									<input type="password" class="form-control" name="password_again" placeholder="Password Again" required>
+									<i class="fa fa-lock"></i> </span>
+							</div>
+							<div class="form-group">
+								<div class="checkbox clip-check check-primary">
+									<input type="checkbox" id="agree" value="agree">
+									<label for="agree">
+										I agree
+									</label>
+								</div>
+							</div>
+							<div class="form-actions">
+								<p>
+									Already have an account?
+									<a href="user-login.php">
+										Log-in
+									</a>
+								</p>
+								<button type="submit" class="btn btn-primary pull-right" id="submit" name="submit">
+									Submit <i class="fa fa-arrow-circle-right"></i>
+								</button>
+							</div>
+						</fieldset>
+					</form>
+
+					<div class="copyright">
+						&copy; <span class="current-year"></span><span class="text-bold text-uppercase"> HMS</span>. <span>All rights reserved</span>
+					</div>
+
+				</div>
+
+			</div>
+		</div>
+
+	</body>
+	<!-- end: BODY -->
+</html>
